2c6ac8ec15dfed5e3aa07e7fc41ba74ea9abe4dd
Shihab177 <shihab7777h@gmail.com>
2026-03-01 22:48:59 +0600

    [Refactor]: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-
-
-
+import Navbar from "@/components/home/Navbar";
+import { Toaster } from "react-hot-toast";
+import NextAuthProvider from "@/Provider/NextAuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,8 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
-        {children}
+        <NextAuthProvider>
+          <Navbar />
+          <Toaster />
+          {children}
+        </NextAuthProvider>
       </body>
     </html>
   );
